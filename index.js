28d5e9982dbd96ec02fa01d0fab820fe2df1db90
andrestrw <133chicagobulls@gmail.com>
2024-03-08 15:39:22 +0100

    ADD: Second version APiiiiiIiiiiiiiiiii

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ app.use(cors());
 
 app.use(express.json());
 
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 mongoose
   .connect(URL_CONNECT)
 
