8db8b85cfcc112c336d339b066cbdbd39f7e4a1e
andrestrw <133chicagobulls@gmail.com>
2024-03-10 09:57:57 +0100

    Agregando url del origen -/ 1

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 
-app.options("*", cors(corsOptions));
+// app.options("*", cors(corsOptions));
 
 // app.all("/*", function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
