2b8c3fdd2ec524f44e76ec269f179355d7ff5cbb
andrestrw <133chicagobulls@gmail.com>
2024-03-21 22:22:41 +0100

    despliegue2

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ app.use(express.json());
 
 app.use(express.static("dist"));
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
 
 mongoose
   .connect(URL_CONNECT)
